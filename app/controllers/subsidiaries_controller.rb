f01e400f56200190b6851eeef492ff2b267ff7ad
marcosvrrs <marcosvrrs@gmail.com>
2019-12-02 13:06:31 -0300

    Testes do fds feitos 2/12/2019

--- app/controllers/subsidiaries_controller.rb
+++ app/controllers/subsidiaries_controller.rb
@@ -5,4 +5,43 @@ def index
 
 end
 
+def new
+
+  @subsidiary = Subsidiary.new
+
+end
+
+def show
+
+@subsidiaries = Subsidiary.find(params[:id])
+
+end
+
+def create
+
+  @subsidiary = Subsidiary.create(params.require(:subsidiary).permit(:name, :cnpj, :adress))
+  if @subsidiary.save!
+    redirect_to subsidiaries_path
+  end
+
+
+end
+
+def update
+  @subsidiary = Subsidiary.find(params[:id])
+  if @subsidiary.update(params.require(:subsidiary).permit(:name, :cnpj, :adress ))
+    redirect_to subsidiaries_path
+  end
+
+end
+
+def edit
+
+  @subsidiary = Subsidiary.find(params[:id])
+
+
+
+end
+
+
 end
